8854efd2bb2f038a9898a07dba6f3cab639b34ad
Mega4869 <101839703+Mega4869@users.noreply.github.com>
2025-02-22 00:03:35 -0300

    pqp
    testei coisas há dias e só agora vi que não havia desfeito

--- scripts/airship_control.gd
+++ scripts/airship_control.gd
@@ -1,14 +1,18 @@
 extends Node
 
+
 @onready var airship := Global.airship
 @onready var player := Global.player
-@onready var area: InteractionArea = $Interact
+var interact_parent: Interact
 
-func _ready():
-	area.interact = Callable(self, "_on_interact")
+
+func _ready() -> void:
+	# Connect to parent interact component
+	interact_parent = get_parent()
+	interact_parent.interacted.connect(_on_interact)
+
 
 func _on_interact() -> void:
 	# Toggle between player movement and airship movement
 	player.can_move = !player.can_move
 	airship.controlling = !airship.controlling
-	player.last_interact = area if airship.controlling else null
