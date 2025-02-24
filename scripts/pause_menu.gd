8cee6c831d241e8e5580d1f57b23c5f08d0eb92a
Gabriel <gbrlferz@gmail.com>
2025-02-24 20:21:09 -0300

    pause menu fixed + naming convention

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -13,12 +13,14 @@ func _ready() -> void:
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
-		visible = !visible
+		visible = true
+		get_tree().paused = true
 		pause_main_menu.visible = true
 		settings_menu.visible = false
 
 
 func _on_resume_button_pressed() -> void:
+	get_tree().paused = false
 	visible = false
 
 
